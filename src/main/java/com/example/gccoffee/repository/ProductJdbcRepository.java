e04100acbb9f48c362ab8ef20f9e48ac69eb9154
hyunseo <goatsfish@gmail.com>
2022-11-21 14:30:07 +0900

    feat : 상품 전체 검색 기능 추가

---
+++ src/main/java/com/example/gccoffee/repository/ProductJdbcRepository.java
@@ -0,0 +1,70 @@
+package com.example.gccoffee.repository;
+
+import com.example.gccoffee.model.Category;
+import com.example.gccoffee.model.Product;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static com.example.gccoffee.JdbcUtils.*;
+
+@Repository
+public class ProductJdbcRepository implements ProductRepository {
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public ProductJdbcRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return jdbcTemplate.queryForObject("select * from products", productRowMapper);
+    }
+
+    @Override
+    public Product insert(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product update(Product product) {
+        return null;
+    }
+
+    @Override
+    public Optional<Product> findById(UUID productId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Product> findByName(String productName) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Product> findByCategory(Category category) {
+        return null;
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    private static final RowMapper<Product> productRowMapper = (resultSet, i) -> {
+        var productId = toUUID(resultSet.getBytes("product_id"));
+        var produceName = resultSet.getString("produce_name");
+        var category = Category.valueOf(resultSet.getString("category"));
+        var price = resultSet.getLong("price");
+        var description = resultSet.getString("description");
+        var createdAt = toLocalDateTime(resultSet.getTimestamp("created_at"));
+        var updatedAt = toLocalDateTime(resultSet.getTimestamp("updated_at"));
+        return new Product(productId, produceName, category, price, description, createdAt, updatedAt);
+    };
+
+}
